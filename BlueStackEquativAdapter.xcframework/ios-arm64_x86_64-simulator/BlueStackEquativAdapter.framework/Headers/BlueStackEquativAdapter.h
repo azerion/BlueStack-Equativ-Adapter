a9d0d18ff7e85090cb788c25a378c4cf96bfd329
Bishnu Pada Chanda <bishnu.chanda@ext.azerion.com>
2025-04-21 19:53:23 +0600

    Updated BlueStackEquativAdapter xcframework file

---
+++ BlueStackEquativAdapter.xcframework/ios-arm64_x86_64-simulator/BlueStackEquativAdapter.framework/Headers/BlueStackEquativAdapter.h
@@ -0,0 +1,18 @@
+//
+//  BlueStackEquativAdapter.h
+//  BlueStackEquativAdapter
+//
+//  Created by Bishnu Pada Chanda on 12/3/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BlueStackEquativAdapter.
+FOUNDATION_EXPORT double BlueStackEquativAdapterVersionNumber;
+
+//! Project version string for BlueStackEquativAdapter.
+FOUNDATION_EXPORT const unsigned char BlueStackEquativAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlueStackEquativAdapter/PublicHeader.h>
+
+
